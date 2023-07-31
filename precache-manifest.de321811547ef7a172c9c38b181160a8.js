d9089fa354989da05ec7ffcf7fbc8c59abf61ad0
Kornebari <your@email.here>
2023-07-31 15:19:57 -0500

    Updates

---
+++ precache-manifest.de321811547ef7a172c9c38b181160a8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87299fe8c8a037b68de4d4e2486306e6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ca23511ab58350ab4a3e",
+    "url": "./static/css/2.3eb2a443.chunk.css"
+  },
+  {
+    "revision": "0c7600f8c2f286ddbf79",
+    "url": "./static/css/main.3835392f.chunk.css"
+  },
+  {
+    "revision": "ca23511ab58350ab4a3e",
+    "url": "./static/js/2.aef58148.chunk.js"
+  },
+  {
+    "revision": "334a3166bc9976dda67650e8c6f88652",
+    "url": "./static/js/2.aef58148.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c7600f8c2f286ddbf79",
+    "url": "./static/js/main.19b3092e.chunk.js"
+  },
+  {
+    "revision": "b3287891d2c1f1cb6915",
+    "url": "./static/js/runtime-main.590b0372.js"
+  },
+  {
+    "revision": "c80cfc9ca2ba9763939c20f20ac8bf62",
+    "url": "./static/media/frame2.c80cfc9c.png"
+  }
+]);
