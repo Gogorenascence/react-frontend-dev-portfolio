fbca07387619c0a57a6cd96ac43c5f23318b6c44
Kornebari <your@email.here>
2023-08-02 16:12:28 -0500

    Lightswitch change

--- src/components/Switch.js
+++ src/components/Switch.js
@@ -1,149 +1,43 @@
 import React, { Component } from "react";
-import Switch from "react-switch";
 
 class LightSwitch extends Component {
-  titles = [];
-
   constructor() {
     super();
-    this.state = { checked: false };
+    this.state = { checked: false, dark: false };
     this.onThemeSwitchChange = this.onThemeSwitchChange.bind(this);
   }
 
   onThemeSwitchChange(checked) {
-    this.setState({ checked });
+    this.setState((prevState) => ({
+      checked,
+      dark: !prevState.dark, // Toggle the dark property
+    }));
     this.setTheme();
   }
 
   setTheme() {
     var dataThemeAttribute = "data-theme";
     var body = document.body;
-    var newTheme =
-      body.getAttribute(dataThemeAttribute) === "dark" ? "light" : "dark";
+    var newTheme = this.state.dark ? "light" : "dark";
     body.setAttribute(dataThemeAttribute, newTheme);
   }
 
   render() {
+    const iconSrc = this.state.dark
+      ? "https://i.imgur.com/7qbG6Ln.png"
+      : "https://i.imgur.com/binFIkR.png";
+
     return (
-
-
-            <div className="lightswitch">
-
-              <Switch
-                checked={this.state.checked}
-                onChange={this.onThemeSwitchChange}
-                offColor="#7293ff"
-                onColor="#353535"
-                className="react-switch mx-auto"
-                width={90}
-                height={40}
-                uncheckedIcon={
-                  <span
-                    className="iconify"
-                    data-icon="twemoji:owl"
-                    data-inline="false"
-                    style={{
-                      display: "block",
-                      height: "100%",
-                      fontSize: 25,
-                      textAlign: "end",
-                      marginLeft: "20px",
-                      color: "#7293ff",
-                    }}
-                  ></span>
-                }
-                checkedIcon={
-                  <span
-                    className="iconify"
-                    data-icon="noto-v1:sun-with-face"
-                    data-inline="false"
-                    style={{
-                      display: "block",
-                      height: "100%",
-                      fontSize: 25,
-                      textAlign: "end",
-                      marginLeft: "10px",
-                      color: "#353239",
-                    }}
-                  ></span>
-                }
-                id="icon-switch"
-              />
-
-          </div>
-
-
+      <div className="lightswitch">
+        <img
+          onClick={this.onThemeSwitchChange}
+          className="lightbutton"
+          src={iconSrc}
+          alt={this.state.dark ? "sun icon" : "moon icon"}
+        />
+      </div>
     );
   }
 }
-
-
-
-// import React, { useState } from "react";
-// import Switch from "react-switch";
-
-// function LightSwitch() {
-//     const [checked, setChecked] = useState(false);
-
-//     const handleChange = (event) => {
-//         setChecked(true);
-//         setTheme();
-//     };
-
-//     const setTheme = (event) => {
-//         var dataThemeAttribute = "data-theme";
-//         var body = document.body;
-//         var newTheme =
-//         body.getAttribute(dataThemeAttribute) === "dark" ? "light" : "dark";
-//         body.setAttribute(dataThemeAttribute, newTheme);
-//     }
-
-//     return (
-//         <div className="lightswitch">
-//             <Switch
-//                 checked={checked}
-//                 onClick={handleChange}
-//                 offColor="#99aef3"
-//                 onColor="#353535"
-//                 className="react-switch mx-auto"
-//                 width={90}
-//                 height={40}
-//                 zIndex={100}
-//                 uncheckedIcon={
-//                   <span
-//                     className="iconify"
-//                     data-icon="twemoji:owl"
-//                     data-inline="false"
-//                     style={{
-//                       display: "block",
-//                       height: "100%",
-//                       fontSize: 25,
-//                       textAlign: "end",
-//                       marginLeft: "20px",
-//                       color: "#353239",
-//                       zIndex: "100",
-//                     }}
-//                   ></span>
-//                 }
-//                 checkedIcon={
-//                   <span
-//                     className="iconify"
-//                     data-icon="noto-v1:sun-with-face"
-//                     data-inline="false"
-//                     style={{
-//                       display: "block",
-//                       height: "100%",
-//                       fontSize: 25,
-//                       textAlign: "end",
-//                       marginLeft: "10px",
-//                       color: "#353239",
-//                     }}
-//                   ></span>
-//                 }
-//                 id="icon-switch"
-//               />
-//         </div>
-//     );
-// }
 
 export default LightSwitch;
